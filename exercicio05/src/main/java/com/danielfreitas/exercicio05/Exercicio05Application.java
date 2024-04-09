0ac22af62982ffda4b3a9c44057b84ec8f67a527
danielfreitassc <danielfreitassc@gmail.com>
2024-04-08 23:38:39 -0300

    Feat: Organização

--- exercicio05/src/main/java/com/danielfreitas/exercicio05/Exercicio05Application.java
+++ exercicio05/src/main/java/com/danielfreitas/exercicio05/Exercicio05Application.java
@@ -3,7 +3,11 @@ package com.danielfreitas.exercicio05;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Execicio intermediario",version = "1", description = "Api desnvolvida para materia de backend"))
 public class Exercicio05Application {
 
 	public static void main(String[] args) {
